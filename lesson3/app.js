57c7f8a5b1065b81423556444823ce12d7f53cb4
youjie <youjie@hudongpai.com>
2018-04-27 21:39:32 +0800

    爬虫

---
+++ lesson3/app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const superagent = require("superagent");
+const cheerio = require("cheerio");
+app.get("/", (req, res, next) => {
+  superagent.get("https://cnodejs.org/").end((err, sres) => {
+    if (err) {
+      return next(err);
+    }
+    //sres.text是网页内容
+    let $ = cheerio.load(sres.text);
+    let items = [];
+    $("#topic_list .topic_title").each(function(idx, element) {
+      var $element = $(element);
+      items.push({
+        title: $element.attr("title"),
+        href: $element.attr("href")
+      });
+    });
+
+    res.send(items);
+  });
+});
+app.listen(3001, () => {
+  console.log("监听3001端口");
+});
