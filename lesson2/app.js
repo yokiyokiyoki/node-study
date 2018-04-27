400801216c3fb2e49edc15dfac63ec7c5c403ac5
youjie <youjie@hudongpai.com>
2018-04-27 15:28:52 +0800

    注释

--- lesson2/app.js
+++ lesson2/app.js
@@ -5,8 +5,9 @@ const app = express();
 app.get("/", (req, res) => {
   let q = req.query.q;
   let md5Value = utility.md5(q);
-  res.send(q);
+  res.send(md5Value);
 });
 app.listen(3001, () => {
   console.log("监听3001端口");
 });
+//http://localhost:3001/?q=yoki
