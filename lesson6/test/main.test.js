418a7311d535e10312602b87f9ebd635bd2e9efa
youjie <youjie@hudongpai.com>
2018-04-28 17:02:28 +0800

    describle

--- lesson6/test/main.test.js
+++ lesson6/test/main.test.js
@@ -1,6 +1,6 @@
 const main = require("../main");
 const should = require("should");
-desrcible("./main.test", () => {
+describle("./main.test", () => {
   it("should equal 55 when n===10", () => {
     main.fibonacci(10).should.equal(55);
   });
